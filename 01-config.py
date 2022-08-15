150a8b38a3860b8de2ce7ab5fde71172fccd1164
jakir-ruet <jakir.ruet.bd@gmail.com>
2022-08-15 14:34:43 +0600

    creating mongo client done

--- 01-config.py
+++ 01-config.py
@@ -1,6 +1,22 @@
 from pymongo import MongoClient
 
-demoClient = MongoClient()
+mgClient = MongoClient()
 myClient = MongoClient('mongodb://localhost:27017/')
-myDbConfig = myClient['mymongodb']
-myCollConfig = myDbConfig['mycollection']
+myDbConfig = myClient['dbMongodb']
+myCollConfig = myDbConfig['myCollection']
+
+myList = [
+    {
+        'id': 1,
+        'name': 'Jakir',
+        'email': 'jakir@gmail.com'
+    }
+]
+
+x = myCollConfig.insert_many(myList)
+dbList = myClient.list_database_names()
+if input('Enter database ') in dbList:
+    print('The database exists')
+else:
+    print('No database here')
+print(dbList)
